999309b6c540164a0745dfb47415124a5c7244b5
Vivever <myselfvivek9@gmail.com>
2019-06-12 23:29:59 +0530

    Design Profile Page

---
+++ app/src/main/java/com/pistudiosofficial/myclass/AdapterCourseItem.java
@@ -0,0 +1,63 @@
+package com.pistudiosofficial.myclass;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+public class AdapterCourseItem extends RecyclerView.Adapter<AdapterCourseItem.ViewHolder>{
+
+    private static final String Tag="Recycler View Adapter";
+
+    private ArrayList<String> mCourseNames=new ArrayList<>();
+    private ArrayList<String> mInstructorNames=new ArrayList<>();
+    private Context mContext;
+
+    public AdapterCourseItem(ArrayList<String> mCourseNames, ArrayList<String> mInstructorNames, Context mContext) {
+        this.mCourseNames = mCourseNames;
+        this.mInstructorNames = mInstructorNames;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG,"OnCreateView Holder Called");
+
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.course_listitem,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Log.d(TAG,"OnBindView Holder Called");
+        holder.CourseName.setText(mCourseNames.get(position));
+        holder.InstructorName.setText(mInstructorNames.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCourseNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView CourseName,InstructorName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            CourseName=itemView.findViewById(R.id.courseName);
+            InstructorName=itemView.findViewById(R.id.instructorName);
+        }
+    }
+}
--- app/src/main/java/com/pistudiosofficial/myclass/activities/ProfileActivity.java
+++ app/src/main/java/com/pistudiosofficial/myclass/activities/ProfileActivity.java
@@ -1,32 +1,53 @@
 package com.pistudiosofficial.myclass.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pistudiosofficial.myclass.AdapterCourseItem;
 import com.pistudiosofficial.myclass.Common;
 import com.pistudiosofficial.myclass.R;
 import com.pistudiosofficial.myclass.presenter.ProfilePresenter;
 import com.pistudiosofficial.myclass.view.ProfileActivityView;
 
+import java.util.ArrayList;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
 public class ProfileActivity extends AppCompatActivity implements ProfileActivityView {
+
+    private static final String Tag=" ProfileActivity";
+    ImageButton backButton;
+    private ArrayList<String> mCourseNames=new ArrayList<>();
+    private ArrayList<String> mInstructorNames=new ArrayList<>();
 
     ProfilePresenter presenter;
     TextView addHOD;
     EditText hodEmail;
     Button add;
+    private ArrayList<String> mAttendancePercentage=new ArrayList<>();
     Dialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        courseAndInstructorNames();
+
+        backButton=findViewById(R.id.backButton);
+//        AttendancePercentage();
         if(Common.CURRENT_USER.AdminLevel.equals("admin")) {
             addHOD = findViewById(R.id.tv_add_hod);
             addHOD.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +71,12 @@ public class ProfileActivity extends AppCompatActivity implements ProfileActivit
             addHOD = findViewById(R.id.tv_add_hod);
             addHOD.setVisibility(View.GONE);
         }
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this,MainActivity.class));
+            }
+        });
     }
 
     @Override
@@ -61,7 +88,6 @@ public class ProfileActivity extends AppCompatActivity implements ProfileActivit
         }
 
     }
-
 
     @Override
     public void hodAddSuccess() {
@@ -79,5 +105,54 @@ public class ProfileActivity extends AppCompatActivity implements ProfileActivit
         addHOD.setText(text);
     }
 
+    public void courseAndInstructorNames()
+    {
+        mCourseNames.add("Graphics");
+        mInstructorNames.add("Badal Soni");
 
+        mCourseNames.add("Maths");
+        mInstructorNames.add("K.N.Das");
+
+        mCourseNames.add("FLAT");
+        mInstructorNames.add("Shyampada Mukherjee");
+
+        mCourseNames.add("Signals And System");
+        mInstructorNames.add("Anish KUmar");
+
+        initRecyclerViewForCourse();
+    }
+//    public void AttendancePercentage()
+//    {
+//        mCourseNames.add("Graphics");
+//        mAttendancePercentage.add("89");
+//
+//        mCourseNames.add("Maths");
+//        mAttendancePercentage.add("91.5");
+//
+//        mCourseNames.add("FLAT");
+//        mAttendancePercentage.add("79");
+//
+//        mCourseNames.add("Signals And System");
+//        mAttendancePercentage.add("72");
+//
+//        initRecyclerViewForAttendance();
+//    }
+    private void initRecyclerViewForCourse(){
+        Log.d(TAG,"Recycler View Initialized");
+
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        RecyclerView recyclerView=findViewById(R.id.course_recyclerview);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        AdapterCourseItem adapterCourseItem= new AdapterCourseItem(mCourseNames,mInstructorNames,this);
+        recyclerView.setAdapter(adapterCourseItem);
+    }
+//    private void initRecyclerViewForAttendance(){
+//        Log.d(TAG,"Recycler View Initialized");
+//
+//        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+//        RecyclerView recyclerView=findViewById(R.id.attendance_recyclerview);
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//        AdapterCourseItem adapterCourseItem= new AdapterCourseItem(mCourseNames,mAttendancePercentage,this);
+//        recyclerView.setAdapter(adapterCourseItem);
+//    }
 }
